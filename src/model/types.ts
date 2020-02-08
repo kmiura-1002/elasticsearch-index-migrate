589ff99fdb474dc034c6a6cfe34e01fc5821a189
kmiura <miura.koki.hoge@gmail.com>
2020-02-09 00:35:36 +0900

    type追加

--- src/model/types.ts
+++ src/model/types.ts
@@ -30,9 +30,60 @@ export enum Migrationtype {
     CREATE_INDEX = 'CREATE_INDEX'
 }
 
+export enum MigrationType {
+    INDEX = 'INDEX',
+    MAPPING = 'MAPPING',
+    SETTING = 'SETTING',
+    TEMPLATE = 'TEMPLATE'
+}
+
 export type MigrationScript = {
     type: Migrationtype;
     index_name: string;
     description: string;
     migrate_script: any;
+};
+export type MigrationExecutor = {
+    execute(): void;
+};
+
+export type ResolvedMigration = {
+    getVersion(): string;
+    getDescription(): string;
+    getScript(): string;
+    getChecksum(): number;
+    getType(): MigrationType;
+    getPhysicalLocation(): string;
+    getExecutor(): MigrationExecutor;
+};
+
+export type AppliedMigration = {
+    installedRank: number;
+    version: string;
+    description: string;
+    type: MigrationType;
+    script: string;
+    installedOn: Date;
+    installedBy: string;
+    executionTime: number;
+    success: boolean;
+};
+
+export type MigrationInfoContext = {
+    outOfOrder: boolean;
+    pending: boolean;
+    missing: boolean;
+    ignored: boolean;
+    future: boolean;
+    target: string;
+    schema: string;
+    baseline: string;
+    lastResolved: string;
+    lastApplied: string;
+};
+
+export type MigrationInfo = {
+    resolvedMigration: ResolvedMigration;
+    appliedMigration: AppliedMigration;
+    context: MigrationInfoContext;
 };
