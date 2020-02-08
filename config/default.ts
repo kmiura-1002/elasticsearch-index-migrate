5f71837cd0ccf59791571edfd1a3034d3e4c7805
kmiura <miura.koki.hoge@gmail.com>
2020-02-08 22:33:34 +0900

    設定追加

--- config/default.ts
+++ config/default.ts
@@ -2,5 +2,8 @@ export default {
     elasticsearch: {
         version: '7.5.2',
         host: 'http://localhost:9202'
+    },
+    migration: {
+        locations: ['migration', 'mapping']
     }
 };
