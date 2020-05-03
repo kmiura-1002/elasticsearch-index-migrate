a059f363ee9a229459d0640e174c4cb2d2b4105a
kmiura <miura.koki.hoge@gmail.com>
2020-05-03 23:53:44 +0900

    add char

--- src/AbstractCommand.ts
+++ src/AbstractCommand.ts
@@ -8,48 +8,56 @@ export const DefaultOptions = {
         multiple: true,
         required: false,
         env: 'ELASTICSEARCH_MIGRATION_LOCATIONS',
+        char: 'L',
         description:
             'Read the migration file from the directory set in the $ELASTICSEARCH_MIGRATION_LOCATIONS environment variable'
     }),
     baseline_version: flags.string({
         required: false,
         env: 'ELASTICSEARCH_MIGRATION_BASELINE_VERSION',
+        char: 'B',
         description:
             'Migrate from the baseline set in the ELASTICSEARCH_MIGRATION_BASELINE_VERSION environment variable'
     }),
     elasticsearch_version: flags.string({
         required: false,
         env: 'ELASTICSEARCH_VERSION',
+        char: 'V',
         description:
             'Run migration with Elasticsearch version set in ELASTICSEARCH_VERSION environment variable'
     }),
     elasticsearch_host: flags.string({
         required: false,
         env: 'ELASTICSEARCH_HOST',
+        char: 'H',
         description:
             'Connect to Elasticsearch with the value set in the ELASTICSEARCH_HOST environment variable'
     }),
     elasticsearch_ssl: flags.string({
         required: false,
         env: 'ELASTICSEARCH_SSL',
+        char: 'S',
         description:
             'Connect to Elasticsearch with the value set in the ELASTICSEARCH_SSL environment variable'
     }),
     elasticsearch_cloudid: flags.string({
         required: false,
         env: 'ELASTICSEARCH_CLOUDID',
+        char: 'C',
         description:
             'Connect to Elasticsearch with the value set in the ELASTICSEARCH_CLOUDID environment variable'
     }),
     elasticsearch_username: flags.string({
         required: false,
         env: 'ELASTICSEARCH_USERNAME',
+        char: 'U',
         description:
             'Connect to Elasticsearch with the value set in the ELASTICSEARCH_USERNAME environment variable'
     }),
     elasticsearch_password: flags.string({
         required: false,
         env: 'ELASTICSEARCH_PASSWORD',
+        char: 'P',
         description:
             'Connect to Elasticsearch with the value set in the ELASTICSEARCH_PASSWORD environment variable'
     }),
