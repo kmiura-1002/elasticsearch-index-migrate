03a45ddfd595229088b7f6f26c10a38de8a9daba
kmiura <miura.koki.hoge@gmail.com>
2020-06-07 15:05:20 +0900

    add test

--- test/commands/abstractCommand.test.ts
+++ test/commands/abstractCommand.test.ts
@@ -218,4 +218,186 @@ describe('abstract command test', () => {
         .it(
             'The error occurs if username and password are not provided when specifying the cloudId.'
         );
+
+    test.stub(EsUtils, 'default', sinon.stub().returns(new MockElasticsearchClient()))
+        .stub(
+            fileUtils,
+            'findAllFiles',
+            sinon.stub().callsFake((dir: string[]) => {
+                const paths: string[] = [];
+                dir.forEach((value) => {
+                    findFiles(value, (data) => paths.push(data));
+                });
+                return paths;
+            })
+        )
+
+        .env({
+            ELASTICSEARCH_MIGRATION_LOCATIONS: `${process.cwd()}/test/data/migration`,
+            ELASTICSEARCH_MIGRATION_BASELINE_VERSION:
+                'test_ELASTICSEARCH_MIGRATION_BASELINE_VERSION',
+            ELASTICSEARCH_VERSION: 'test_ELASTICSEARCH_VERSION',
+            ELASTICSEARCH_HOST: 'http://0.0.0.0:9200/test_ELASTICSEARCH_HOST'
+        })
+        .stdout()
+        .command(['plan', '-i', 'test1'])
+        .it('Must be able to connect to Elasticsearch via host', (ctx) => {
+            const findAllFilesStub = findAllFiles as sinon.SinonStub;
+            const esClientStub = getElasticsearchClient as sinon.SinonStub;
+            expect(process.env.ELASTICSEARCH_MIGRATION_LOCATIONS).to.equal(
+                `${process.cwd()}/test/data/migration`
+            );
+            expect(process.env.ELASTICSEARCH_MIGRATION_BASELINE_VERSION).to.equal(
+                'test_ELASTICSEARCH_MIGRATION_BASELINE_VERSION'
+            );
+            expect(process.env.ELASTICSEARCH_VERSION).to.equal('test_ELASTICSEARCH_VERSION');
+            expect(process.env.ELASTICSEARCH_HOST).to.equal(
+                'http://0.0.0.0:9200/test_ELASTICSEARCH_HOST'
+            );
+            expect(process.env.ELASTICSEARCH_SSL).to.equal(undefined);
+            expect(process.env.ELASTICSEARCH_CLOUDID).to.equal(undefined);
+            expect(process.env.ELASTICSEARCH_USERNAME).to.equal(undefined);
+            expect(process.env.ELASTICSEARCH_PASSWORD).to.equal(undefined);
+
+            expect(findAllFilesStub.calledWith([`${process.cwd()}/test/data/migration`])).is.true;
+            expect(
+                esClientStub.calledWith({
+                    version: 'test_ELASTICSEARCH_VERSION',
+                    connect: {
+                        host: 'http://0.0.0.0:9200/test_ELASTICSEARCH_HOST',
+                        sslCa: undefined,
+                        cloudId: undefined,
+                        username: undefined,
+                        password: undefined
+                    }
+                })
+            ).is.true;
+            expect(ctx.stdout).to.contain(
+                'Version Description Type      Installedon State   \n' +
+                    'v1.0.0  description ADD_FIELD             PENDING \n'
+            );
+        });
+
+    test.stub(EsUtils, 'default', sinon.stub().returns(new MockElasticsearchClient()))
+        .stub(
+            fileUtils,
+            'findAllFiles',
+            sinon.stub().callsFake((dir: string[]) => {
+                const paths: string[] = [];
+                dir.forEach((value) => {
+                    findFiles(value, (data) => paths.push(data));
+                });
+                return paths;
+            })
+        )
+
+        .env({
+            ELASTICSEARCH_MIGRATION_LOCATIONS: `${process.cwd()}/test/data/migration`,
+            ELASTICSEARCH_MIGRATION_BASELINE_VERSION:
+                'test_ELASTICSEARCH_MIGRATION_BASELINE_VERSION',
+            ELASTICSEARCH_VERSION: 'test_ELASTICSEARCH_VERSION',
+            ELASTICSEARCH_HOST: 'http://0.0.0.0:9200/test_ELASTICSEARCH_HOST',
+            ELASTICSEARCH_SSL: 'test_ELASTICSEARCH_SSL'
+        })
+        .stdout()
+        .command(['plan', '-i', 'test1'])
+        .it('Must be able to connect to Elasticsearch via host and ssl.', (ctx) => {
+            const findAllFilesStub = findAllFiles as sinon.SinonStub;
+            const esClientStub = getElasticsearchClient as sinon.SinonStub;
+            expect(process.env.ELASTICSEARCH_MIGRATION_LOCATIONS).to.equal(
+                `${process.cwd()}/test/data/migration`
+            );
+            expect(process.env.ELASTICSEARCH_MIGRATION_BASELINE_VERSION).to.equal(
+                'test_ELASTICSEARCH_MIGRATION_BASELINE_VERSION'
+            );
+            expect(process.env.ELASTICSEARCH_VERSION).to.equal('test_ELASTICSEARCH_VERSION');
+            expect(process.env.ELASTICSEARCH_HOST).to.equal(
+                'http://0.0.0.0:9200/test_ELASTICSEARCH_HOST'
+            );
+            expect(process.env.ELASTICSEARCH_SSL).to.equal('test_ELASTICSEARCH_SSL');
+            expect(process.env.ELASTICSEARCH_CLOUDID).to.equal(undefined);
+            expect(process.env.ELASTICSEARCH_USERNAME).to.equal(undefined);
+            expect(process.env.ELASTICSEARCH_PASSWORD).to.equal(undefined);
+
+            expect(findAllFilesStub.calledWith([`${process.cwd()}/test/data/migration`])).is.true;
+            expect(
+                esClientStub.calledWith({
+                    version: 'test_ELASTICSEARCH_VERSION',
+                    connect: {
+                        host: 'http://0.0.0.0:9200/test_ELASTICSEARCH_HOST',
+                        sslCa: 'test_ELASTICSEARCH_SSL',
+                        cloudId: undefined,
+                        username: undefined,
+                        password: undefined
+                    }
+                })
+            ).is.true;
+            expect(ctx.stdout).to.contain(
+                'Version Description Type      Installedon State   \n' +
+                    'v1.0.0  description ADD_FIELD             PENDING \n'
+            );
+        });
+
+    test.stub(EsUtils, 'default', sinon.stub().returns(new MockElasticsearchClient()))
+        .stub(
+            fileUtils,
+            'findAllFiles',
+            sinon.stub().callsFake((dir: string[]) => {
+                const paths: string[] = [];
+                dir.forEach((value) => {
+                    findFiles(value, (data) => paths.push(data));
+                });
+                return paths;
+            })
+        )
+
+        .env({
+            ELASTICSEARCH_MIGRATION_LOCATIONS: `${process.cwd()}/test/data/migration`,
+            ELASTICSEARCH_MIGRATION_BASELINE_VERSION:
+                'test_ELASTICSEARCH_MIGRATION_BASELINE_VERSION',
+            ELASTICSEARCH_VERSION: 'test_ELASTICSEARCH_VERSION',
+            ELASTICSEARCH_CLOUDID: 'test_ELASTICSEARCH_CLOUDID',
+            ELASTICSEARCH_USERNAME: 'test_ELASTICSEARCH_USERNAME',
+            ELASTICSEARCH_PASSWORD: 'test_ELASTICSEARCH_PASSWORD'
+        })
+        .stdout()
+        .command(['plan', '-i', 'test1'])
+        .it(
+            'Must be able to connect to Elasticsearch with cloud id, user_name and password.',
+            (ctx) => {
+                const findAllFilesStub = findAllFiles as sinon.SinonStub;
+                const esClientStub = getElasticsearchClient as sinon.SinonStub;
+                expect(process.env.ELASTICSEARCH_MIGRATION_LOCATIONS).to.equal(
+                    `${process.cwd()}/test/data/migration`
+                );
+                expect(process.env.ELASTICSEARCH_MIGRATION_BASELINE_VERSION).to.equal(
+                    'test_ELASTICSEARCH_MIGRATION_BASELINE_VERSION'
+                );
+                expect(process.env.ELASTICSEARCH_VERSION).to.equal('test_ELASTICSEARCH_VERSION');
+                expect(process.env.ELASTICSEARCH_HOST).to.equal(undefined);
+                expect(process.env.ELASTICSEARCH_SSL).to.equal(undefined);
+                expect(process.env.ELASTICSEARCH_CLOUDID).to.equal('test_ELASTICSEARCH_CLOUDID');
+                expect(process.env.ELASTICSEARCH_USERNAME).to.equal('test_ELASTICSEARCH_USERNAME');
+                expect(process.env.ELASTICSEARCH_PASSWORD).to.equal('test_ELASTICSEARCH_PASSWORD');
+
+                expect(findAllFilesStub.calledWith([`${process.cwd()}/test/data/migration`])).is
+                    .true;
+                expect(
+                    esClientStub.calledWith({
+                        version: 'test_ELASTICSEARCH_VERSION',
+                        connect: {
+                            host: undefined,
+                            sslCa: undefined,
+                            cloudId: 'test_ELASTICSEARCH_CLOUDID',
+                            username: 'test_ELASTICSEARCH_USERNAME',
+                            password: 'test_ELASTICSEARCH_PASSWORD'
+                        }
+                    })
+                ).is.true;
+                expect(ctx.stdout).to.contain(
+                    'Version Description Type      Installedon State   \n' +
+                        'v1.0.0  description ADD_FIELD             PENDING \n'
+                );
+            }
+        );
 });
