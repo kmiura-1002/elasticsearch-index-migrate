1a231296ed317f36b2d18d68e1ae1577a1130117
kmiura <miura.koki.hoge@gmail.com>
2022-03-07 00:39:49 +0900

    add flags in plan command

--- src/app/commands/plan/index.ts
+++ src/app/commands/plan/index.ts
@@ -1,4 +1,4 @@
-import { Command } from '@oclif/core';
+import { Command, Flags } from '@oclif/core';
 import { DefaultFlags, esConnectionFlags } from '../../config/flags/defaultCommandFlags';
 import { createMigrationIndex } from '../../decorators/createMigrationIndex';
 import { migrateLock } from '../../decorators/migrateLock';
@@ -9,7 +9,13 @@ export default class Plan extends Command {
 
     static flags = {
         ...DefaultFlags,
-        ...esConnectionFlags
+        ...esConnectionFlags,
+        ignoredMigrations: Flags.boolean({
+            default: false,
+            env: 'IGNORED_MIGRATIONS',
+            description:
+                'Migration target for additions made during the already migrated version.\nFor example, use this option when you want to migrate v2.0.0 when v1.0.0 and v3.0.0 have already been migrated. Normally, this will result in an ingnore status. Setting this option to true will make v2.0.0 the next migration version after v3.0.0.'
+        })
     };
 
     static args = [...DefaultArgs];
