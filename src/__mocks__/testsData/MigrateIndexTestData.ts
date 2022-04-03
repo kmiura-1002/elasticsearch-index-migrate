d7424e0e38962010686a61bc64a9ffbec7533514
kmiura <miura.koki.hoge@gmail.com>
2022-04-03 23:22:49 +0900

    fix test data

--- src/__mocks__/testsData/MigrateIndexTestData.ts
+++ src/__mocks__/testsData/MigrateIndexTestData.ts
@@ -1,50 +1,56 @@
-import { format } from 'date-fns';
 import { MigrationTypes } from '../../app/types';
 import type { MigrateIndex } from '../../app/types';
 
-export const migrateIndices = (installed_on: Date): MigrateIndex[] => [
-    {
-        script_name: 'v1.0.0__test',
-        migrate_version: 'v1.0.0',
-        description: '',
-        execution_time: 1,
-        index_name: 'test',
-        installed_on: format(installed_on, "yyyy-MM-dd'T'HH:mm:ss"),
-        script_type: MigrationTypes.CREATE_INDEX,
-        success: true,
-        checksum: undefined
-    },
-    {
-        script_name: 'v1.0.0__test',
-        migrate_version: 'v1.1.1',
-        description: '',
-        execution_time: 1,
-        index_name: 'test',
-        installed_on: format(installed_on, "yyyy-MM-dd'T'HH:mm:ss"),
-        script_type: MigrationTypes.ADD_FIELD,
-        success: true,
-        checksum: undefined
-    },
-    {
-        script_name: 'v1.0.0__test',
-        migrate_version: 'v1.20.0',
-        description: '',
-        execution_time: 1,
-        index_name: 'test',
-        installed_on: format(installed_on, "yyyy-MM-dd'T'HH:mm:ss"),
-        script_type: MigrationTypes.ADD_FIELD,
-        success: true,
-        checksum: undefined
-    },
-    {
-        script_name: 'v1.0.0__test',
-        migrate_version: 'v1.10.2',
-        description: '',
-        execution_time: 1,
-        index_name: 'test',
-        installed_on: format(installed_on, "yyyy-MM-dd'T'HH:mm:ss"),
-        script_type: MigrationTypes.ADD_FIELD,
-        success: false,
-        checksum: undefined
+export const migrateIndices = (size?: number): MigrateIndex[] => {
+    if (size) {
+        if (size < 0) {
+            throw new Error('Size must be greater than zero.');
+        }
+        Array.from({ length: size }, (_x, i) => ({
+            script_name: `v1.0.${i}__add_field.json`,
+            migrate_version: `v1.0.${i}`,
+            description: 'book index',
+            execution_time: 1,
+            index_name: 'test',
+            installed_on: "2020-01-01'T'00:00:00",
+            script_type: MigrationTypes.ADD_FIELD,
+            success: true,
+            checksum: undefined
+        }));
     }
-];
+    return [
+        {
+            script_name: 'v1.0.0__create_index.json',
+            migrate_version: 'v1.0.0',
+            description: 'book index',
+            execution_time: 1,
+            index_name: 'test',
+            installed_on: "2020-01-01'T'00:00:00",
+            script_type: MigrationTypes.CREATE_INDEX,
+            success: true,
+            checksum: undefined
+        },
+        {
+            script_name: 'v1.0.1__add_field.json',
+            migrate_version: 'v1.0.1',
+            description: 'book index',
+            execution_time: 1,
+            index_name: 'test',
+            installed_on: "2020-01-01'T'00:00:00",
+            script_type: MigrationTypes.ADD_FIELD,
+            success: true,
+            checksum: undefined
+        },
+        {
+            script_name: 'v1.0.2__add_fieldcopy.json',
+            migrate_version: 'v1.0.2',
+            description: 'book index',
+            execution_time: 1,
+            index_name: 'test',
+            installed_on: "2020-01-01'T'00:00:00",
+            script_type: MigrationTypes.ADD_FIELD,
+            success: true,
+            checksum: undefined
+        }
+    ];
+};
