8434fffb80bfe55a66e2aa51d8feb1a223380ca2
kmiura <miura.koki.hoge@gmail.com>
2022-06-03 00:10:56 +0900

    update jest

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,11 @@ module.exports = {
     testMatch: [
         '<rootDir>/src/**/__tests__/**/*.{js,ts}',
         '<rootDir>/src/**/*.{spec,test,it}.{js,ts}'
-    ]
+    ],
+    moduleFileExtensions: ["js", "mjs", "cjs", "jsx", "ts", "tsx", "json", "node", "jsm"],
+    // Added to resolve error:'ENOENT: no such file or directory'
+    moduleNameMapper: {
+        'es6/lib/(.*)':'<rootDir>/node_modules/es6/lib/$1',
+        'es7/lib/(.*)':'<rootDir>/node_modules/es7/lib/$1'
+    }
 };
