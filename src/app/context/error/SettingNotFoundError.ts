851ca6a5c37b712fd1da6a132533b0f396f13880
kmiura <miura.koki.hoge@gmail.com>
2022-06-19 18:34:41 +0900

    add error classes

---
+++ src/app/context/error/SettingNotFoundError.ts
@@ -0,0 +1,3 @@
+import { DomainError } from './DomainError';
+
+export class SettingNotFoundError extends DomainError {}
---
+++ src/app/context/error/UnsupportedVersionError.ts
@@ -0,0 +1,3 @@
+import { DomainError } from './DomainError';
+
+export class UnsupportedVersionError extends DomainError {}
