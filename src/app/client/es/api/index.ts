0346b2ee4c5c8b0d844a45e4fb2e1f033392b742
kmiura <miura.koki.hoge@gmail.com>
2022-07-03 23:45:58 +0900

    refactor: encapsulate es client

--- src/app/client/es/api/index.ts
+++ src/app/client/es/api/index.ts
@@ -1,14 +1,56 @@
+/**
+ * @package
+ */
 export { healthCheckApi } from './healthCheckApi';
+/**
+ * @package
+ */
 export { putMappingApi } from './putMappingApi';
+/**
+ * @package
+ */
 export { createIndexApi } from './createIndexApi';
+/**
+ * @package
+ */
 export { searchApi } from './searchApi';
+/**
+ * @package
+ */
 export { existsApi } from './existsApi';
+/**
+ * @package
+ */
 export { putSettingApi } from './putSettingApi';
+/**
+ * @package
+ */
 export { deleteApi } from './deleteApi';
+/**
+ * @package
+ */
 export { postDocumentApi } from './postDocumentApi';
+/**
+ * @package
+ */
 export { closeConnection } from './closeConnection';
+/**
+ * @package
+ */
 export { getMappingApi } from './getMappingApi';
+/**
+ * @package
+ */
 export { getIndexApi } from './getIndexApi';
+/**
+ * @package
+ */
 export { deleteDocumentsApi } from './deleteDocumentsApi';
+/**
+ * @package
+ */
 export { countApi } from './countApi';
+/**
+ * @package
+ */
 export { deleteDocumentApi } from './deleteDocumentApi';
